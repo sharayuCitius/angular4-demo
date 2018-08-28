360484dfab06c0dcc876c02920ed1883b72193ba
Sharayu Shah <SharayuS@citiustech.com>
2018-08-28 15:29:55 +0530

    Seventh Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FootballersListComponent } from '../app/footballers-list/footballers-list.component'
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,FootballersListComponent
       ],
     }).compileComponents();
   }));
